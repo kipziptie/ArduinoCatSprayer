8033d3c68bed59e0c1b2022852441bf9a1528f7b
Kip Trujillo <kiptrujillo@protonmail.com>
2020-06-04 16:18:33 -0700

    Clean up an empty directory

--- Servo_sender/Servo_sender.ino
+++ Servo_sender/Servo_sender.ino
@@ -5,6 +5,7 @@
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
+  Serial.begin(9600);
 }
 byte send = 0;
 
@@ -12,8 +13,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   updatePosition(0);
   delay(1000);
+  send = 90;
   updatePosition(90);
   delay(1000);
+  send=180;
   updatePosition(180);
   delay(1000);
 }
@@ -23,4 +26,5 @@ void updatePosition(int position){
     Wire.beginTransmission(4);
     Wire.write(position);
     Wire.endTransmission();
+    Serial.println(position);
   }
