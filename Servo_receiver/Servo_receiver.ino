78ffbaa15ab41e8c89a0eaa4425277176f3192d1
Kip <kiptrujillo@protonmail.com>
2020-06-04 16:41:39 -0700

    Add Servo controller code

---
+++ Servo_receiver/Servo_receiver.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+#include <Wire.h>
+Servo servo;
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(3);
+  Wire.begin(4);
+  Wire.onReceive(ServoEvent);
+  Serial.begin(9600);
+  updateServo(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);
+}
+
+void ServoEvent(int howMany){
+    while( 1 <= Wire.available() ){ // loop through all bytes but the last
+      int c = Wire.read();
+      Serial.println(c);
+      updateServo(c);
+    }
+  
+  }
+
+void updateServo(int position){
+    servo.write(position);
+    
+  }
