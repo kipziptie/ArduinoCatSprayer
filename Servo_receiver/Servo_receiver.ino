c99be3fd0afd10f324be318d5e011a3de1c402d5
Kip <kiptrujillo@protonmail.com>
2020-06-08 14:59:53 -0700

    added tuple processing on arduino controlling servo positioning

--- Servo_receiver/Servo_receiver.ino
+++ Servo_receiver/Servo_receiver.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 Servo servo;
 String message;
+ 
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,7 +23,7 @@ void loop() {
 }
 
 void ServoEvent(int howMany){
-    
+int arr[2];    
     while( 1 <= Wire.available() ){ // loop through all bytes but the last
       char c = Wire.read();
       Serial.println(c);
@@ -31,6 +32,7 @@ void ServoEvent(int howMany){
       //updateServo(c);
     }
     Serial.println(message);
+    unpackMessage(message, arr);
     message = "";
   }
 
@@ -42,7 +44,19 @@ void updateServo2(int position){
   
   }
 
-void unpackMessage(String MSG, int *arr){
-  
+void unpackMessage(String MSG, int arr[2]){
+  //Split the incoming message up to the ',' comma delimiter, 
+  //omitting the opening bracket
+  // the toInt() function at the end stores this cropped string as an integer
+  arr[0] = MSG.substring(1, MSG.indexOf(",") ).toInt();
 
+  // Split the incoming message from the ',' comma delimiter UP to 
+  // and omitting the closing bracket. 
+  // the toInt() function at the end stores this cropped string as an integer
+  arr[1] = MSG.substring(MSG.indexOf(",")+2, MSG.indexOf(")")).toInt();
+
+Serial.print("index0:");
+Serial.print(arr[0]+1);
+Serial.print(" index1:");
+Serial.println(arr[1]+1);
   }
